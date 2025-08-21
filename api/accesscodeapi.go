d12caff3ddfd1ed1ec602fee535674d5154eb32d
megautam18 <prabhu.gautam14@gmail.com>
2025-08-21 22:52:41 +0530

    Completion of viewing api

---
+++ api/accesscodeapi.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+type acrequest struct {
+	AccessCode string `json:"code"`
+}
+
+func req(w http.ResponseWriter, r *http.Request) {
+	/*client:=&http.Client{
+		Timeout:30*time.Second,
+	}*/
+	if r.Method != http.MethodPost {
+		http.Error(w, "Incorrect method", http.StatusMethodNotAllowed)
+		return
+	}
+	var reqdata acrequest
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&reqdata)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	acode := os.Getenv("access_code")
+	if reqdata.AccessCode == acode {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, "Invalid access code", http.StatusUnauthorized)
+	}
+
+}
---
+++ api/getnotes.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type note struct {
+	Content string `json:"content"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	cxt := context.Background()
+	dburl := os.Getenv("db_url")
+	if dburl == "" {
+		http.Error(w, "Database link failed", http.StatusInternalServerError)
+		log.Print("Database link failed")
+		return
+	}
+	dbobj, err := sql.Open("postgres", dburl)
+
+	if err != nil {
+		http.Error(w, "Database disruption", http.StatusInternalServerError)
+		log.Print("Database disruption")
+		return
+	}
+	defer dbobj.Close()
+	var content string
+
+	sc_err := dbobj.QueryRowContext(cxt, "SELECT content FROM notepad WHERE id=1").Scan(&content)
+	note := note{Content: content}
+	if sc_err == sql.ErrNoRows {
+		note.Content = ""
+	}
+	if sc_err != nil {
+		http.Error(w, "Content not present in the database", http.StatusInternalServerError)
+		log.Print("Content not present in the database")
+		return
+	}
+	jsonResponse, json_err := json.Marshal(note)
+
+	if json_err != nil {
+		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
+		log.Print("Failed to marshal JSON")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+}
